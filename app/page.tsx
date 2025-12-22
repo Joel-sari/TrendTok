42771ac97629a5819aa82f29ba15fdd469ecee4b
Joel <Joelsari19@gmail.com>
2025-12-22 16:07:59 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// We have to use rsc for our intial layout to come out
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <Button>Click ME!</Button>
+           Page
+        </div>
+    );
+};
+
+export default Page;
