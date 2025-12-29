e8dffcee140ee1aa672ac1ac187b58c70eed543e
Joel <Joelsari19@gmail.com>
2025-12-29 11:59:26 -0500

    fixing minor bugs

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import Header from "@/components/header";
-import Home from "@/app/(root)/page";
 
-//Accpe
 const Layout = ({ children}: {children: React.ReactNode}) => {
     return (
         <main className="min-h-screen text-gray-400">
             {/* We will add a header component later on here */}
             <Header />
             <div className="container py-10">
-                <Home/>
+                {children}
             </div>
-            Layout
+
         </main>
     );
 };
